a7b6460ef0736004a8caaf0a5e14b152859daf08
dongjingtao <dongjingtao86@gmail.com>
2026-07-06 17:51:04 +0900

    gateway configuration

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GateWayApplication.java
@@ -0,0 +1,16 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: ${董靖涛}
+ * Date: 2026/7/6
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+
+}
